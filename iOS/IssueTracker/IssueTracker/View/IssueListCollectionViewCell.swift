3fe82cea38873e8e06b2669a881f2c4bd47650d5
Sungmin Park <rnfxl92@naver.com>
2020-10-26 16:42:20 +0900

    [feat]IssueListEditViewController 추가

--- iOS/IssueTracker/IssueTracker/View/IssueListCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/View/IssueListCollectionViewCell.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class IssueListCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var mileStoneLabel: UIButton!
+    @IBOutlet weak var labelLabel: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+}
+
+extension UICollectionViewCell {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
 }
---
+++ iOS/IssueTracker/IssueTracker/ViewController/IssueListEditViewController.swift
@@ -0,0 +1,25 @@
+//
+//  IssueListEditViewController.swift
+//  IssueTracker
+//
+//  Created by 박성민 on 2020/10/26.
+//
+
+import UIKit
+
+class IssueListEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func selectAllButtonDidTouch(_ sender: Any) {
+    
+    }
+    
+    @IBAction func cancelButtonDidTouch(_ sender: Any) {
+        
+    }
+}
