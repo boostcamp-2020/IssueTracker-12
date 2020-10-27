11f005e5ebdccf3f4b42e9eee8067bbacb30cc13
Sungmin Park <rnfxl92@naver.com>
2020-10-27 19:47:29 +0900

    [feat] Button image 수정

---
+++ iOS/IssueTracker/IssueTracker/Extensions/UIView+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+Extension.swift
+//  IssueTracker
+//
+//  Created by 박성민 on 2020/10/27.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map(UIColor.init)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+    }
+}
+
