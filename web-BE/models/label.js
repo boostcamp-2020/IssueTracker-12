e6cd5e8eee33ac555a97c1ac5ea61cf599ad304a
JungSWon <sw.onandon@gmail.com>
2020-10-28 19:36:29 +0900

    [feat] 레이블 API 작성중
    routes/api/label
    - index.js router 구성
    - controller.js 메소드 작성 create, read, update, delete
    models/label.js
    - laberlModel 모듈 작성 insert, select, update, delete

---
+++ web-BE/models/label.js
@@ -0,0 +1,17 @@
+// const connection = require('../config/db_connection');
+// const query = require('../util/query');
+
+const labelModel = {
+    insert : ()=>{
+        return true
+    },
+    select : ()=>{
+        return true
+    },
+    update : ()=>{
+        return true
+    },
+    delete : ()=>{
+        return true
+    }
+}
---
+++ web-BE/routes/api/label/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const labelController = require('./controller')
+
+router.post('/', labelController.create) 
+router.get('/', labelController.read)
+router.patch('/', labelController.update)
+router.delete('/', labelController.delete)
+
+module.exports = router
