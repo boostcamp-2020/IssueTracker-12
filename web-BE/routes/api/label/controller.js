031378d19e9511ee343a128e1689052234db01d1
JungSWon <sw.onandon@gmail.com>
2020-10-29 13:10:55 +0900

    [feat] label api CRUD controller 작성 1차 완료 (협의필요사항있음)
    delete부 TODO 참고 부탁드려요
    - 삭제할 라벨 id 를 쿼리로 받아도 괜찮을지
    - 그렇다면 다른 api delete도 쿼리로 통일하면 좋겠어요
    - 쿼리 변수명은 임의로 labelid 했습니다. labelid vs labelId vs 외 제안

--- web-BE/routes/api/label/controller.js
+++ web-BE/routes/api/label/controller.js
@@ -2,16 +2,20 @@ const labelModel = require('../../../models/label');
 
 const labelController = {
   create: (req, res) => {
-    labelModel.insert();
+    labelModel.insert(req.body);
+    res.json({message:"create a label successfully"})
   },
   read: async (req, res) => {
-    labelModel.select();
+    const laberArr = await labelModel.select();
+    res.json({ lableArr = lableArr })
   },
   update: (req, res) => {
-    labelModel.update();
+    labelModel.update(req.body);
+    res.json({message:"update the label successfully"})
   },
   delete: (req, res) => {
-    labelModel.delete();
+    // TODO : 협의필 : labelid 변수명을 쿼리로 받는 것으로 우선 가정하겠습니다.
+    labelModel.delete(parseInt(req.query.labelid)); 
   },
 };
 
