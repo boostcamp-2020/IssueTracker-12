464aade8c02e66d6a19193a989271a8364ae06ff
SeunghyoKu <seunghyoku@gmail.com>
2020-10-29 21:22:40 +0900

    [feat] milestone model 작성 - config/query.js에서 sql문을 가져와 insert/select/update/delete 작업

---
+++ web-BE/models/milestone.js
@@ -0,0 +1,38 @@
+const connection = require('../config/db_connection');
+const sql = require('../config/query');
+
+const milestoneModel = {
+  insert: async (title, dueDate, content) => {
+    try {
+      const insertInfo = [title, dueDate, content];
+      connection.query(sql.insertMilestone, insertInfo);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  select: async () => {
+    try {
+      const res = await connection.query(sql.selectMilestone);
+      return res[0];
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  update: async (milestoneid, title, dueDate, content) => {
+    try {
+      const updateInfo = [title, dueDate, content, milestoneid];
+      connection.query(sql.updateMilestone, updateInfo);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  delete: async (milestoneid) => {
+    try {
+      connection.query(sql.deleteMilestone, milestoneid);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
+
+module.exports = milestoneModel;
