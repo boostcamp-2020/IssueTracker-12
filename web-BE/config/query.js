5486166e087649d3b44584c97ecd9772568b7aa3
SeunghyoKu <seunghyoku@gmail.com>
2020-10-29 21:06:38 +0900

    [feat] milestone CRUD 관련 query문 작성 - insert, select, update, delete 하는 쿼리문 작성

--- web-BE/config/query.js
+++ web-BE/config/query.js
@@ -2,6 +2,14 @@ module.exports = {
   // label
   insertLabel: 'INSERT INTO Label (name,description,color) VALUE(?,?,?);',
   selectLabel: 'SELECT * FROM Label',
-  updateLabel: 'UPDATE Label SET name=?, description=?, color=?, WHERE label_id=?;',
+  updateLabel:
+    'UPDATE Label SET name=?, description=?, color=?, WHERE label_id=?;',
   deleteLabel: 'DELETE FROM Label WHERE label_id=?;',
+  // milestone
+  insertMilestone:
+    'INSERT INTO Milestone (title,due_date,content) VALUES (?,?,?);',
+  selectMilestone: 'SELECT * FROM Milestone;',
+  updateMilestone:
+    'UPDATE Milestone SET title=?, due_date=?, content=? WHERE milestone_id=?;',
+  deleteMilestone: 'DELETE FROM Milestone WHERE milestone_id=?;',
 };
