5c95d822c8643156934b39f3a000adc1fd501214
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-10-30 13:33:12 +0900

    [feat] authModel 중 select 구현
    username과 social을 가지고 DB에 유저가 있는지 확인을 합니다.
    query 결과 배열의 첫번째 항만 select의 result에 해당하므로
    const [res]
    로 비구조화 해서 필요한 값만 받았습니다.
    그 이후에 select의 결과는 배열이므로 res[0]만 return 했습니다.

---
+++ web-BE/models/auth.js
@@ -0,0 +1,16 @@
+const connection = require('../config/db_connection');
+const sql = require('../config/query');
+
+const authModel = {
+  insert: () => true,
+  select: async (username, social) => {
+    try {
+      const [res] = await connection.query(sql.selectUser, [username, social]);
+      return res[0];
+    } catch (err) { console.error(err); }
+  },
+  update: () => true,
+  delete: () => true,
+};
+
+module.exports = authModel;
