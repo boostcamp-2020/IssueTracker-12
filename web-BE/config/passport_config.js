5db3cafbdfad8b78af89aea90ab6173ddc5674e6
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-10-30 13:26:46 +0900

    [feat] passport github, apple 소셜 로그인에 대항하는 유저가 있는지 확인하는 기능 추가
    각 passport strategy에는 config 객체와 verify 함수가 들어갑니다.
    
    config에서 stretegy의 옵션을 설정하고 verify에서 해당하는 유저가 있는지 확인합니다.
    
    verify 함수에서 social 값은 자동으로 넘어오지 않기 때문에 passportVerify 함수를 클로저로 만들고 social 값을 추가해서 각 소셜 로그인에 맞는 verify 함수를 추가하도록 했습니다.

---
+++ web-BE/config/passport_config.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const LocalStrategy = require('passport-local').Strategy;
+const userModel = require('../models/auth');
+
+require('dotenv').config();
+
+const passportLocalConfig = {
+  usernameField: 'username',
+  passwordField: 'password',
+  session: false,
+};
+
+const passportVerify = (social) => async (username, password, done) => {
+  const user = await userModel.select(username, social);
+  if (!user) {
+    return done(null, false);
+  }
+  return done(null, user);
+};
+
+const passportGithubVerify = passportVerify('github');
+const passportAppleVerify = passportVerify('apple');
+
+const jwtConfig = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+const jwtVerify = async (jwtPayload, done) => {
+  const user = await userModel.select(jwtPayload.username);
+  if (!user) {
+    return done(null, false);
+  }
+  return done(null, user);
+};
+
+const passportGithubStrategy = new LocalStrategy(passportLocalConfig, passportGithubVerify);
+const passportAppleStrategy = new LocalStrategy(passportLocalConfig, passportAppleVerify);
+const jwtStrategy = new JWTStrategy(jwtConfig, jwtVerify);
+
+module.exports = () => {
+  passport.use('githubLocal', passportGithubStrategy);
+  passport.use('appleLocal', passportAppleStrategy);
+  passport.use('jwt', jwtStrategy);
+};
