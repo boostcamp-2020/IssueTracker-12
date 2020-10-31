61344e1fcab0b807000d10c115f615e8eab67343
Sungmin Park <rnfxl92@naver.com>
2020-10-31 10:20:24 +0900

    [feat] label colorpicker 제거
    label preview label 로 수정

--- iOS/IssueTracker/IssueTracker/Label/ViewController/LabelEditViewController.swift
+++ iOS/IssueTracker/IssueTracker/Label/ViewController/LabelEditViewController.swift
@@ -12,23 +12,11 @@ class LabelEditViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var colorTextField: UITextField!
-    @IBOutlet weak var colorPickerButton: UIButton!
+    @IBOutlet weak var labelPreviewLabel: PaddedLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-    }
-
-    @IBAction func colorPickerButtonDidTouch(_ sender: Any) {
-        presentColorPicker()
-    }
-    
-    private func presentColorPicker() {
-        let colorPicker = UIColorPickerViewController()
-        colorPicker.delegate = self
-        colorPicker.selectedColor = colorPickerButton.backgroundColor ?? UIColor.systemTeal
-        colorPicker.title = "Color Picker"
-        self.present(colorPicker, animated: true, completion: nil)
     }
     
     @IBAction func resetButtonDidTouch(_ sender: Any) {
@@ -47,15 +35,3 @@ class LabelEditViewController: UIViewController {
     }
 }
 
-extension LabelEditViewController: UIColorPickerViewControllerDelegate {
-    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
-        
-    }
-    
-    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
-        DispatchQueue.main.async { [weak self] in
-            self?.colorTextField.text = viewController.selectedColor.toHexString()
-            self?.colorPickerButton.backgroundColor = viewController.selectedColor
-        }
-    }
-}
