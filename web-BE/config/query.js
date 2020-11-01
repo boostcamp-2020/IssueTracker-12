72cb6cf7b676915a3786346c4ae4aa42bfa8ead3
SeunghyoKu <seunghyoku@gmail.com>
2020-11-01 23:11:30 +0900

    [style] 쿼리문 가독성 높이기 - 기존에 SELECT * 로 작성하였던 부분을 - 칼럼을 명확히 작성하여 가독성을 높이게 되었습니다.

--- web-BE/config/query.js
+++ web-BE/config/query.js
@@ -1,15 +1,15 @@
 module.exports = {
   // label
-  insertLabel: 'INSERT INTO Label (name,description,color) VALUE(?,?,?);',
-  selectLabel: 'SELECT * FROM Label',
+  insertLabel: "INSERT INTO Label (name,description,color) VALUE(?,?,?);",
+  selectLabel: "SELECT * FROM Label",
   updateLabel:
-    'UPDATE Label SET name=?, description=?, color=?, WHERE label_id=?;',
-  deleteLabel: 'DELETE FROM Label WHERE label_id=?;',
+    "UPDATE Label SET name=?, description=?, color=?, WHERE label_id=?;",
+  deleteLabel: "DELETE FROM Label WHERE label_id=?;",
   // milestone
   insertMilestone:
-    'INSERT INTO Milestone (title,due_date,content) VALUES (?,?,?);',
-  selectMilestone: 'SELECT * FROM Milestone;',
+    "INSERT INTO Milestone (title,due_date,content) VALUES (?,?,?);",
+  selectMilestone: "SELECT milestone_id,title,due_date,content FROM Milestone;",
   updateMilestone:
-    'UPDATE Milestone SET title=?, due_date=?, content=? WHERE milestone_id=?;',
-  deleteMilestone: 'DELETE FROM Milestone WHERE milestone_id=?;',
+    "UPDATE Milestone SET title=?, due_date=?, content=? WHERE milestone_id=?;",
+  deleteMilestone: "DELETE FROM Milestone WHERE milestone_id=?;",
 };
