55aa8fd0d3f6894acbc4b8e7d037a6d75854102d
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-02 16:32:35 +0900

    [fix] query 구분 주석을 추가
    query를 구분할 수 있는 주석을 추가했습니다.

--- web-BE/config/query.js
+++ web-BE/config/query.js
@@ -4,5 +4,6 @@ module.exports = {
   selectLabel: 'SELECT * FROM Label',
   updateLabel: 'UPDATE Label SET name=?, description=?, color=?, WHERE label_id=?;',
   deleteLabel: 'DELETE FROM Label WHERE label_id=?;',
+  // user
   selectUser: 'SELECT user_id, username, password, social FROM User WHERE username=? and social=?',
 };
