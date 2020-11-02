e80064660eb0f258a2de1a241be145b9de1086e4
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-02 20:40:16 +0900

    [refactor] 띄어쓰기 후 쉼표가 있는 문제 해결
    eslint 상에서 쉼표 띄어쓰기 순으로 쓰기를 권장하고 있어서 eslint 설정 그대로 반영했습니다.

--- web-BE/routes/api/label/controller.js
+++ web-BE/routes/api/label/controller.js
@@ -2,17 +2,17 @@ const labelModel = require('../../../models/label');
 
 const labelController = {
   create: async (req, res) => {
-    const { name, description ,color } = req.body;
-    const insertId = await labelModel.insert([name, description ,color]);
-    res.status(200).json({insertId})
+    const { name, description, color } = req.body;
+    const insertId = await labelModel.insert([name, description, color]);
+    res.status(200).json({ insertId });
   },
   read: async (req, res) => {
     const labelArr = await labelModel.select();
     res.status(200).json({ labelArray: labelArr });
   },
   update: (req, res) => {
-    const { name, description ,color } = req.body;
-    labelModel.update([name, description ,color, parseInt(req.params.labelid)]);
+    const { name, description, color } = req.body;
+    labelModel.update([name, description, color, parseInt(req.params.labelid)]);
     res.status(200).json({ message: 'update the label successfully' });
   },
   delete: (req, res) => {
