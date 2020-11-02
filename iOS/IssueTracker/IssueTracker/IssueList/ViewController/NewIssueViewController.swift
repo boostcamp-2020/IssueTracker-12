630cdffb96b68209db5cf245833a0b3dc2293fe4
Sungmin Park <rnfxl92@naver.com>
2020-11-02 17:32:48 +0900

    [feat] NewIssue UI 구현
    -pod MarkdownKit 추가

---
+++ iOS/IssueTracker/IssueTracker/IssueList/ViewController/NewIssueViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewIssueViewController.swift
+//  IssueTracker
+//
+//  Created by 박성민 on 2020/11/02.
+//
+
+import UIKit
+
+class NewIssueViewController: UIViewController {
+
+    @IBOutlet weak var issueNumberLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var issueContentTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
