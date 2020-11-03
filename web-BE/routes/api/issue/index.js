ed6463a9c261995b0621d545d3ceea166d291e1f
j03y14 <j03y14@gmail.com>
2020-11-03 16:32:37 +0900

    [feat] issue router의 get api 구현
    issue router에서 GET '/' 하면 read 하도록 했습니다.

--- web-BE/routes/api/issue/index.js
+++ web-BE/routes/api/issue/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const issueController = require('./controller');
 
 router.post('/', issueController.create);
-// router.get('/', issueController.read);
+router.get('/', issueController.read);
 // router.put('/:labelid', issueController.update);
 // router.delete('/:labelid', issueController.delete);
 
