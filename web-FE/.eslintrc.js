6bb5ab4991dc85b807b8d63845582a107793051c
Seunghyo <seunghyoku@gmail.com>
2020-11-03 15:04:54 +0900

    [chore] eslint 설치 - airbnb 기반 eslint 설치하기

---
+++ web-FE/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "eslint:recommended", "airbnb/hooks"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  ignorePatterns: ["node_modules/"],
+  rules: {},
+};
