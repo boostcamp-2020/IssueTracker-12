9a003f184a30e9ab9c01b105bd5742c30aceec1a
j03y14 <j03y14@gmail.com>
2020-11-04 17:38:43 +0900

    [feat] comment router get api 추가
    GET /api/issue/:issueId/comment로 들어오는 요청에 대해서 처리하도록 구현했습니다.

--- web-BE/routes/api/issue/comment/index.js
+++ web-BE/routes/api/issue/comment/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router({ mergeParams: true });
 const commentController = require('./controller');
 
 router.post('/', commentController.create);
-
+router.get('/', commentController.read);
 
 module.exports = router;
