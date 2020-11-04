a34c7dd34714fa0cd7503b34a2a5c0f5c75575fa
j03y14 <j03y14@gmail.com>
2020-11-04 17:43:26 +0900

    [feat] comment 수정 api 추가
    PATCH /api/issue/:issueId/comment/:commentId url에 대한 api를 추가했습니다.

--- web-BE/routes/api/issue/comment/index.js
+++ web-BE/routes/api/issue/comment/index.js
@@ -3,5 +3,6 @@ const commentController = require('./controller');
 
 router.post('/', commentController.create);
 router.get('/', commentController.read);
+router.patch('/:commentId', commentController.update);
 
 module.exports = router;
