e30ab9213b74245f6149a1b9b5ce1274da5ae16f
JungSWon <sw.onandon@gmail.com>
2020-11-05 18:08:13 +0900

    [merge] Merge web-FE to web

--- web-FE/src/App.jsx
+++ web-FE/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>hello react!</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <h1>HELL no world</h1>
+      <Route path="/" exact={true} component={Home} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
