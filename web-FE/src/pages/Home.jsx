27f19cfaeb08d7a211d9025e9d0358b4406e33b9
JungSWon <sw.onandon@gmail.com>
2020-11-06 02:12:55 +0900

    [refactor] Add src/components/githubLogin - 깃헙 로그인 컴포넌트를 만들어 분리하였다.

--- web-FE/src/pages/Home.jsx
+++ web-FE/src/pages/Home.jsx
@@ -1,25 +1,12 @@
-import React, { useEffect } from "react";
-import { linkToGetCode, getUserInfo, saveUser } from '../api/auth';
+import React from "react";
+import GithubLogin from "../components/githubLogin"
 
 const App = () => {
-  useEffect( async () => {
-    const reg = /.code=(.*)/
-    if(reg.test(window.location)){
-      const code = reg.exec(window.location)[1]
-      const {userInfo, isExistUser} = await getUserInfo(code)
-      if(isExistUser){
-        alert('login되었습니다')
-      }
-      else{
-        await saveUser(userInfo)
-        alert('회원가입되었습니다')
-      }
-    }
-    
-  });
-
-  return <button onClick={linkToGetCode}>GITHUB</button>;
+  return (
+    <div>
+      <GithubLogin />
+    </div>
+  )
 };
-
 
 export default App;
