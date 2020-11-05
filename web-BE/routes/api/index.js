72985539732898c6582fdabb52199846689e8a49
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-05 11:16:55 +0900

    [feat] jwt 토큰으로 인증을 하는 미들웨어 추가
    jwt 토큰으로 인증을 하는 미들웨어를 추가했습니다. auth 이후에 추가해서 auth 라우터가 아니면 모두 인증이 필요하도록 했습니다.

--- web-BE/routes/api/index.js
+++ web-BE/routes/api/index.js
@@ -1,12 +1,13 @@
+const passport = require('passport');
+
 const router = require('express').Router();
 const auth = require('./auth');
-// const comment = require("./comment");
 const issue = require('./issue');
 const label = require('./label');
 const milestone = require('./milestone');
 
 router.use('/auth', auth);
-// router.use("/comment", comment);
+router.use(passport.authenticate('jwt', { session: false }));
 router.use('/issue', issue);
 router.use('/label', label);
 router.use('/milestone', milestone);
