5c6a9703ef5088f9ed9a6f0fa92e7b4854c11bea
JungSWon <sw.onandon@gmail.com>
2020-11-06 01:56:28 +0900

    [refactor] Create `src/api/auth` - auth api 관련 모듈 리팩토링

---
+++ web-FE/src/api/auth.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
+
+export const saveUser = async ({username, social})=>{
+  const apiurl = "http://localhost:8080/api/auth"
+  const { insertUserId } = await axios.post( apiurl, { username, social } )
+}
+
+export const getUserInfo = async (code) => {
+  const apiurl = "http://localhost:8080/api/auth/oauth"
+  try{
+    const result = await axios.get(
+      `${apiurl}?code=${code}&client_id=${clientId}&client_secret=${clientSecret}`
+      );
+    const {userInfo, isExistUser} = result.data
+    return {userInfo, isExistUser}
+  } catch(error){
+    console.error(error)
+  }
+};
+
+export const linkTo = async () => {
+  window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
+};
--- web-FE/src/pages/Home.jsx
+++ web-FE/src/pages/Home.jsx
@@ -1,32 +1,7 @@
 import React, { useEffect } from "react";
-import axios from "axios";
+import { linkTo, getUserInfo, saveUser } from '../api/auth';
 
 const App = () => {
-  const clientId = process.env.CLIENT_ID
-  const clientSecret = process.env.CLIENT_SECRET
-
-  const saveUser = async ({username, social})=>{
-    const apiurl = "http://localhost:8080/api/auth"
-    const { insertUserId } = await axios.post( apiurl, { username, social } )
-  }
-
-  const getUserInfo = async (code) => {
-    const apiurl = "http://localhost:8080/api/auth/oauth"
-    try{
-      const result = await axios.get(
-        `${apiurl}?code=${code}&client_id=${clientId}&client_secret=${clientSecret}`
-        );
-      const {userInfo, isExistUser} = result.data
-      return {userInfo, isExistUser}
-    } catch(error){
-      console.error(error)
-    }
-  };
-
-  const linkTo = async () => {
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
-  };
-
   useEffect( async () => {
     const reg = /.code=(.*)/
     if(reg.test(window.location)){
@@ -34,7 +9,8 @@ const App = () => {
       const {userInfo, isExistUser} = await getUserInfo(code)
       if(isExistUser){
         alert('login되었습니다')
-      }else{
+      }
+      else{
         await saveUser(userInfo)
         alert('회원가입되었습니다')
       }
