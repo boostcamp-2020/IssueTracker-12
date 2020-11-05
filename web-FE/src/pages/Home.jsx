c0d62072f642a6e0fdaac1e1c22ad34853694de4
JungSWon <sw.onandon@gmail.com>
2020-11-05 16:42:48 +0900

    [feat] 클라이언트에서 버튼을 클릭하면 깃헙로그인 후 깃헙 유저 정보가 넘어오도록 함
    linkTo : github 으로부터 code 를 얻기 위한 리다이렉트
    - code는 리다이렉트 된 url에 포함된다. 데이터를 url에 넘겨주는 깃헙의 특이한 리스폰스...
    - github api 리소스서버는 CORS를 허용하지 않는다. 우리 클라이언트에서 axios GET 요청을 보낼 수 없다는 뜻
    - 그래서 GET 요청을 리다이렉트로 처리
    
    getUserInfo : 우리 서버에 api 요청을 보내서 UserInfo 받기
    - 우리 서버는 github oauth api로 요청을 보내서 리스폰스 받은 유저 정보를 클라이언트로 전달한다
     - CORS 해결을 위한 우회
    
    :sob: 멘붕의 시간을 함께 한 짝코딩 메이트 @SeunghyoKu 님께 영광을 돌립니다 :raised_hands:

---
+++ web-FE/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+  const clientId = process.env.CLIENT_ID
+  const clientSecret = process.env.CLIENT_SECRET
+
+  const getUserInfo = async (code) => {
+    const apiurl = "http://localhost:8080/api/oAuth"
+    try{
+      const result = await axios.get(
+        `${apiurl}?code=${code}&client_id=${clientId}&client_secret=${clientSecret}`
+        );
+      const {userInfo} = result.data
+      console.log(userInfo)
+    } catch(error){
+      console.error(error)
+    }
+  };
+
+  const linkTo = async () => {
+    window.location.href =
+      `https://github.com/login/oauth/authorize?client_id=${clientId}`;
+  };
+
+  useEffect(() => {
+    const reg = /.code=(.*)/
+    if(reg.test(window.location)){
+      const code = reg.exec(window.location)[1]
+      localStorage.setItem("test", code);
+      getUserInfo(code)
+    }
+  });
+
+  return <button onClick={linkTo}>GITHUB</button>;
+};
+
+
+export default App;
