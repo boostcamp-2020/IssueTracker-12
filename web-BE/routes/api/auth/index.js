1b3c4fec034f22af1b034b33ae2d345712140719
JungSWon <sw.onandon@gmail.com>
2020-11-06 01:25:17 +0900

    [feat] addUser을 위한 POST router 추가 - POST api/auth - checkUser부는 서버 내에서 호출 되는 모듈로, 라우터에서는 빼도 될 것으로 생각되어 우선 주석처리

--- web-BE/routes/api/auth/index.js
+++ web-BE/routes/api/auth/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const authController = require('./controller');
 
 router.get('/oauth', authController.getUserInfo);
-router.get('/:social/:username', authController.checkUser);
+router.post('/', authController.addUser)
+// router.get('/:social/:username', authController.checkUser);
 
 module.exports = router;
