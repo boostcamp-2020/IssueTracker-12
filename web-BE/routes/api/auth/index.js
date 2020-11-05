fc6de209c2fd4a3f257d2db84dc0f214afc1c0cd
JungSWon <sw.onandon@gmail.com>
2020-11-05 17:21:21 +0900

    [feat] Add oauth route
    GET api/auth/oauth

--- web-BE/routes/api/auth/index.js
+++ web-BE/routes/api/auth/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const authController = require('./controller');
 
+router.get('/oauth', authController.getUserInfo);
 router.get('/:social/:username', authController.checkUser);
 
 module.exports = router;
