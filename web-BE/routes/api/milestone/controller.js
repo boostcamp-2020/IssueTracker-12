03ce0c92cd532f7d4dd9c6317670f6e35f8e843b
Seunghyo <seunghyoku@gmail.com>
2020-11-06 10:45:10 +0900

    [feat] 마일스톤 api에서 이슈 개수보이게끔 기능 추가 - 나중에 수정이 필요함 (read할 때 포함시키기로 결정) - 일단 구현한 바로는 해당 아이디 값에서의 opened (closed) issue, 퍼센트가 출력되게 하였음

--- web-BE/routes/api/milestone/controller.js
+++ web-BE/routes/api/milestone/controller.js
@@ -1,4 +1,4 @@
-const milestoneModel = require('../../../models/milestone');
+const milestoneModel = require("../../../models/milestone");
 
 const milestoneController = {
   create: async (req, res) => {
@@ -6,9 +6,7 @@ const milestoneController = {
     try {
       const insertInfo = [title, dueDate, content];
       const insertId = await milestoneModel.insert(insertInfo);
-      res
-        .status(200)
-        .json({ message: '마일스톤 생성 완료', insertId });
+      res.status(200).json({ message: "마일스톤 생성 완료", insertId });
     } catch (err) {
       res.status(500).json(err);
     }
@@ -18,29 +16,53 @@ const milestoneController = {
       const milestoneArr = await milestoneModel.select();
       res
         .status(200)
-        .json({ message: '마일스톤 읽기 성공', milestoneArray: milestoneArr });
+        .json({ message: "마일스톤 읽기 성공", milestoneArray: milestoneArr });
     } catch (err) {
       res.status(500).json(err);
     }
   },
-  update: (req, res) => {
+  update: async (req, res) => {
     const { title, due_date: dueDate, content } = req.body;
     try {
-      const milestoneid = parseInt(req.params.milestoneid);
+      const milestoneid = +req.params.milestoneid;
       const updateInfo = [title, dueDate, content, milestoneid];
       milestoneModel.update(updateInfo);
-      res.status(200).json({ message: '마일스톤 업데이트 완료' });
+      res.status(200).json({ message: "마일스톤 업데이트 완료" });
     } catch (err) {
       res.status(500).json(err);
     }
   },
-  delete: (req, res) => {
+  delete: async (req, res) => {
     try {
-      milestoneModel.delete(parseInt(req.params.milestoneid));
-      res.status(200).json({ message: '마일스톤 삭제 완료' });
+      milestoneModel.delete(+req.params.milestoneid);
+      console.log(parseInt(req.params.milestoneid));
+      res.status(200).json({ message: "마일스톤 삭제 완료" });
     } catch (err) {
       res.status(500).json(err);
     }
+  },
+  selectIssuesFromMilestone: async (milestoneid) => {
+    //try {
+    // const milestoneid = +req.params.milestoneid;
+    const result = await milestoneModel.selectIssuesFromMilestone(milestoneid);
+    const totalIssue = result.length;
+    let openedIssue = 0;
+    for (let i = 0; i < totalIssue; i++) {
+      if (result[i].is_open === 1) {
+        openedIssue++;
+      }
+    }
+    const issueInfo = {
+      milestoneid,
+      openedIssue,
+      closedIssue: totalIssue - openedIssue,
+      completePercentage: `${Math.round((openedIssue / totalIssue) * 100)}%`,
+    };
+    //res.status(200).json(responseJson);
+
+    // } catch (err) {
+    //   res.status(500).json(err);
+    // }
   },
 };
 
--- web-BE/routes/api/milestone/index.js
+++ web-BE/routes/api/milestone/index.js
@@ -1,9 +1,13 @@
-const router = require('express').Router();
+const router = require('express').Router({ mergeParams: true });
 const milestoneController = require('./controller');
 
 router.post('/', milestoneController.create);
 router.get('/', milestoneController.read);
 router.put('/:milestoneid', milestoneController.update);
 router.delete('/:milestoneid', milestoneController.delete);
+router.get(
+  '/:milestoneid/issues',
+  milestoneController.selectIssuesFromMilestone,
+);
 
 module.exports = router;
