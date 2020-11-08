65feefd2d542efec88db63cae1e9def05577bfb9
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-08 23:42:23 +0900

    [feat] dropdown의 모달 부분 추가
    아직 css 처리와 modal 바깥 부분 클릭 처리가 되지 않았습니다.

---
+++ web-FE/src/components/DropdownModal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DropdownModal = () => {
+  return (
+    <>
+      <details-menu>
+        <a>open issues and pull requests</a>
+        <a>Your issues</a>
+        <a>Your requests</a>
+      </details-menu>
+    </>
+  )
+};
+
+export default DropdownModal;
