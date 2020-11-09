0388e3cfe255849ab84e19f439d2801aaf0efc9c
JungSWon <sw.onandon@gmail.com>
2020-11-09 23:04:43 +0900

    [feat] Add get token api router, controller - POST api/auth/token to get token - getToken 모듈 작성 중 - ref : https://www.stackhoarder.com/2019/07/17/node-js-passport-js-jwt-token-이용해-로그인-구현/

--- web-BE/routes/api/auth/controller.js
+++ web-BE/routes/api/auth/controller.js
@@ -36,10 +36,26 @@ const authController = {
       const userInfo = { username, social:'github' ,url }
       const isExistUser = await authController.checkUser(userInfo)
       res.json({ userInfo, isExistUser });
+  getToken: (req, res) => {
+    passport.authenticate("local", { session: false }, (err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          message: "Something is not right",
+          user: user,
+        });
+      }
     } 
-    catch (error) { next(error); }
- }
-
+      }
+      req.login(user, { session: false }, (err) => {
+        if (err) {
+          res.send(err);
+        }
+        // jwt.sign('token내용', 'JWT secretkey')
+        const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET);
+        return res.json({ user, token });
+      });
+    })(req, res);
+  },
 };
 
 module.exports = authController;
