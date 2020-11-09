1ce410ed049ef8ced08bc836fbffc1f6fd93c466
JungSWon <sw.onandon@gmail.com>
2020-11-09 15:31:04 +0900

    [feat] apply styled-components on githubLoginButton - 깃헙로그인버튼 컴포넌트에 styled-components 적용

--- web-FE/src/components/login/githubLoginButton.jsx
+++ web-FE/src/components/login/githubLoginButton.jsx
@@ -3,6 +3,20 @@ import { linkToGetCode, getUserInfo, saveUser } from '../../api/auth';
 import styled from 'styled-components'
 
 const GithubLogin = () => {
+  const Button = styled.button`
+    font-size: 1em;
+    color : #ffffff;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+    background: papayawhip;
+    &:hover {
+      background: palevioletred;
+    }
+  `;
+
+  useEffect( async () => {
     const reg = /.code=(.*)/
     if(reg.test(window.location)){
       const code = reg.exec(window.location)[1]
