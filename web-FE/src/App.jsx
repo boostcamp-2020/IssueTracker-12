4f7fa236d996f999f35726c45c65f64df3f743c0
j03y14 <j03y14@gmail.com>
2020-11-09 18:43:13 +0900

    [refactor] page 디렉토리를 없애 경로 수정
    pages 안의 내용이 components로 들어갔기 때문에 경로를 수정했습니다.

--- web-FE/src/App.jsx
+++ web-FE/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import IssueListPage from "@Pages/Issue";
-import NewIssuePage from "@Pages/NewIssuePage";
+import IssueListPage from "@Components/Issue";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={IssueListPage} />
-        <Route path="/issue/new" component={NewIssuePage} />
 
       </Switch>
     </BrowserRouter>
