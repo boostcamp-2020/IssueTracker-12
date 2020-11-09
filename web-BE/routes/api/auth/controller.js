9e8974c2c381d87c7fa3ef0bf2c5a13461a9dff7
JungSWon <sw.onandon@gmail.com>
2020-11-09 23:05:05 +0900

    [style] eslint --fix

--- web-BE/routes/api/auth/controller.js
+++ web-BE/routes/api/auth/controller.js
@@ -1,41 +1,48 @@
-const authModel = require('../../../models/auth');
-const axios = require('axios');
+const authModel = require("../../../models/auth");
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 const authController = {
-  addUser: async (req,res)=>{
-    const { username, social } = req.body
-    const insertUserId = await authModel.insert(username,social)
+  addUser: async (req, res) => {
+    const { username, social } = req.body;
+    const insertUserId = await authModel.insert(username, social);
     res.status(200).json({ insertUserId });
   },
 
-  checkUser: async ({username, social, url}) => {
+  checkUser: async ({ username, social, url }) => {
     const user = await authModel.select(username, social);
-    if (!user) return false
-    return true
+    if (!user) return false;
+    return true;
   },
 
   getUserInfo: async (req, res) => {
     const { code, client_id, client_secret } = req.query;
     const getTokenUrl = `https://github.com/login/oauth/access_token`;
-    const getUserDataUrl = 'https://api.github.com/user';
+    const getUserDataUrl = "https://api.github.com/user";
     const reg = /access_token=(.*)&scope/;
 
-    try { 
+    try {
       const result = await axios.post(getTokenUrl, null, {
-        params: { client_id, client_secret, code }
-      })
+        params: { client_id, client_secret, code },
+      });
 
-      const token = reg.exec(result.data)[1]
-      const { data : userData } = await axios({
-        method: 'get',
+      const token = reg.exec(result.data)[1];
+      const { data: userData } = await axios({
+        method: "get",
         url: getUserDataUrl,
-        headers: { Authorization: `token ${token}` }
-      })
+        headers: { Authorization: `token ${token}` },
+      });
 
-      const { login:username, url } = userData
-      const userInfo = { username, social:'github' ,url }
-      const isExistUser = await authController.checkUser(userInfo)
+      const { login: username, url } = userData;
+      const userInfo = { username, social: "github", url };
+      const isExistUser = await authController.checkUser(userInfo);
       res.json({ userInfo, isExistUser });
+    } catch (error) {
+      next(error);
+    }
+  },
+
   getToken: (req, res) => {
     passport.authenticate("local", { session: false }, (err, user) => {
       if (err || !user) {
@@ -43,8 +50,6 @@ const authController = {
           message: "Something is not right",
           user: user,
         });
-      }
-    } 
       }
       req.login(user, { session: false }, (err) => {
         if (err) {
