75cc8abfa9813afbce2db5a45fa300a44011f72e
JungSWon <sw.onandon@gmail.com>
2020-11-09 15:44:05 +0900

    [feat] Router 관리 및 path 추가

--- web-FE/src/App.jsx
+++ web-FE/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./components/Home/index";
+import Login from "./components/Login/Index";
+import Label from "./components/Label/Index";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>HELL no world</h1>
-      <Route path="/" exact={true} component={Home} />
+      <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/label" component={Label} />
     </BrowserRouter>
   );
 };
