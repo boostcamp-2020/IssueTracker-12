b7bcb0e36466166231c964d46f58acbd49c733ab
j03y14 <j03y14@gmail.com>
2020-11-09 10:35:49 +0900

    [fix] name -> label_name
    api를 통일하기 위해서 name을 label_name으로 수정했습니다.

--- web-BE/routes/api/label/controller.js
+++ web-BE/routes/api/label/controller.js
@@ -2,7 +2,7 @@ const labelModel = require('../../../models/label');
 
 const labelController = {
   create: async (req, res) => {
-    const { name, description, color } = req.body;
+    const { label_name : name, description, color } = req.body;
     const insertId = await labelModel.insert([name, description, color]);
     res.status(200).json({ insertId });
   },
@@ -11,7 +11,7 @@ const labelController = {
     res.status(200).json({ labelArray: labelArr });
   },
   update: (req, res) => {
-    const { name, description, color } = req.body;
+    const { label_name : name, description, color } = req.body;
     labelModel.update([name, description, color, parseInt(req.params.labelid)]);
     res.status(200).json({ message: 'update the label successfully' });
   },
