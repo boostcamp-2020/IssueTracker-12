27aee76f2a320e541cc0428f59dc1ddca57e994c
j03y14 <j03y14@gmail.com>
2020-11-10 15:14:45 +0900

    [feat] 유저를 가져오는 api 추가
    모든 유저를 가져오는 api를 추가했습니다.

---
+++ web-FE/src/api/user.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import instance from './instance';
+
+export const getAllUsers = async () => {
+  const url = "/api/user"
+  try{
+    const result = await instance.get(url);
+    const {userArray} = result.data;
+    return userArray;
+  } catch(error){
+    console.error(error)
+  }
+};
