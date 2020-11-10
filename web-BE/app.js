9ca1892a9e5f7460dfbaf304d0ff0bbb909c0ff4
JungSWon <sw.onandon@gmail.com>
2020-11-10 10:52:50 +0900

    [style] eslint --fix

--- web-BE/app.js
+++ web-BE/app.js
@@ -1,26 +1,26 @@
-const createError = require('http-errors');
-const express = require('express');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
-const cors = require('cors');
-const passportConfig = require('./config/passport_config');
+const createError = require("http-errors");
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const cors = require("cors");
+const passportConfig = require("./config/passport_config");
 
-const router = require('./routes/index');
+const router = require("./routes/index");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(cors({origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 passportConfig();
 
-app.use('/', router);
+app.use("/", router);
 
 app.use((req, res, next) => {
   next(createError(404));
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
   res.json(err.message);
@@ -37,5 +37,5 @@ app.use((err, req, res, next) => {
 const server = app.listen(8080, () => {
   const { port } = server.address();
 
-  console.log('Server is working on port', port);
+  console.log("Server is working on port", port);
 });
