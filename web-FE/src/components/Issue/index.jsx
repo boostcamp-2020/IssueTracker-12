6c05144f77c125a79c0cb02ef9952acc653806f3
j03y14 <j03y14@gmail.com>
2020-11-10 15:22:36 +0900

    [feat] 이슈 페이지에서 여러 store를 사용할 수 있도록 수정

--- web-FE/src/components/Issue/index.jsx
+++ web-FE/src/components/Issue/index.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import PageLayout from '@Components/commons/PageLayout';
 import Header from '@Components/commons/Header';
-import IssueMain from './IssueMain';
 import IssueStore from '@Stores/IssueStore';
+import UserStore from '@Stores/UserStore';
+import LabelStore from "@Stores/LabelStore";
+import IssueMain from './IssueMain';
+import MilestoneStore from "../../stores/MilestoneStore";
 
 const IssuePage = () => (
-  <IssueStore>
-    <PageLayout
-      header={<Header />}
-      main={<IssueMain />}
-    />
-  </IssueStore>
+  <UserStore>
+    <MilestoneStore>
+      <LabelStore>
+        <IssueStore>
+          <PageLayout
+            header={<Header />}
+            main={<IssueMain />}
+          />
+        </IssueStore>
+      </LabelStore>
+    </MilestoneStore>
+  </UserStore>
 );
 
 export default IssuePage;
