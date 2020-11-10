affdfcb64d86250040efc3fcfcf52b588ec314e2
j03y14 <j03y14@gmail.com>
2020-11-10 17:17:45 +0900

    [feat] 드랍다운의 스타일 변경

--- web-FE/src/components/commons/DropdownModal.jsx
+++ web-FE/src/components/commons/DropdownModal.jsx
@@ -5,7 +5,9 @@ const DropdownModal = ({list}) => (
   <>
     <DetailsModal>
       {
-        list?.map(v => (<li key={v.id}>{v.title}</li>))
+        list?.map(v => (
+          <Li key={v.id}>{v.title}</Li>
+        ))
       }
     </DetailsModal>
   </>
@@ -14,6 +16,7 @@ const DropdownModal = ({list}) => (
 const DetailsModal = styled.ul`
   display: flex;
   width: 300px;
+  max-height: 600px;
   position: absolute;
   flex-direction: column;
   z-index: 10;
@@ -23,6 +26,16 @@ const DetailsModal = styled.ul`
   box-shadow: grey 0px 0px 4px 0px;
   border-radius: 5px;
 }
+`;
+
+const Li = styled.li`
+  display:flex;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  box-sizing: border-box;
+  padding: 5px 5px;
+  border-bottom: 1px solid lightgrey;
 `;
 
 export default DropdownModal;
