052e34bb601fbfb2168f8880740b4b4e6103cbcb
JungSWon <sw.onandon@gmail.com>
2020-11-10 21:35:22 +0900

    [feat] Create Label index

--- web-FE/src/components/Label/index.jsx
+++ web-FE/src/components/Label/index.jsx
@@ -2,3 +2,11 @@ import React from 'react';
 import PageLayout from '@Components/commons/PageLayout';
 import Header from '@Components/commons/Header';
 import LabelStore from '@Stores/LabelStore';
+import LabelMain from './LabelMain';
+
+const LabelPage = () => (
+  <LabelStore>
+    <PageLayout header={<Header page="Label" />} main={<LabelMain />} />
+  </LabelStore>
+);
+export default LabelPage;
