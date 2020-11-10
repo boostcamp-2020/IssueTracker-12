7cefa4d948c799d2b8b2d3caf21674cb65fac9bf
j03y14 <j03y14@gmail.com>
2020-11-10 15:13:47 +0900

    [feat] label api 추가
    모든 label 가져오는 api 추가

---
+++ web-FE/src/api/label.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import instance from './instance';
+
+export const getAllLabels = async () => {
+  const url = "/api/label"
+  try{
+    const result = await instance.get(url);
+    const {labelArray} = result.data;
+    return labelArray;
+  } catch(error){
+    console.error(error)
+  }
+};
