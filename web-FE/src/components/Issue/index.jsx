7f2c8586b1331309466229c750488085ddd4ec14
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-11 23:53:47 +0900

    [feat] 이슈 페이지에 IssueFilterStore 추가

--- web-FE/src/components/Issue/index.jsx
+++ web-FE/src/components/Issue/index.jsx
@@ -4,18 +4,21 @@ import Header from '@Components/commons/Header';
 import IssueStore from '@Stores/IssueStore';
 import UserStore from '@Stores/UserStore';
 import LabelStore from "@Stores/LabelStore";
-import IssueMain from './IssueMain';
 import MilestoneStore from "@Stores/MilestoneStore";
+import IssueFilterStore from "@Stores/IssueFilterStore";
+import IssueMain from './IssueMain';
 
 const IssuePage = () => (
   <UserStore>
     <MilestoneStore>
       <LabelStore>
         <IssueStore>
-          <PageLayout
-            header={<Header page="Issue"/>}
-            main={<IssueMain />}
-          />
+          <IssueFilterStore>
+            <PageLayout
+              header={<Header page="Issue"/>}
+              main={<IssueMain />}
+            />
+          </IssueFilterStore>
         </IssueStore>
       </LabelStore>
     </MilestoneStore>
