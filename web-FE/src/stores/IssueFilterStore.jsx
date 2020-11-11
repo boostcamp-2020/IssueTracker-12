4bdd6a7d49c61825fe2687263ad45d9d7830e4a1
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-11 23:57:30 +0900

    [feat] 빈 배열로 통일
    null과 [] 중에 하나로 통일해서 드랍다운에서 하나로 처리할 수 있도록 수정

--- web-FE/src/stores/IssueFilterStore.jsx
+++ web-FE/src/stores/IssueFilterStore.jsx
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from 'react';
 export const IssueFilterContext = React.createContext();
 
 const initFilterState = {
-  menuFilter: null,
-  authorFilter: null,
+  menuFilter: [],
+  authorFilter: [],
   labelFilter: [],
-  milestoneFilter: null,
-  assigneeFilter: null,
+  milestoneFilter: [],
+  assigneeFilter: [],
 };
 
 
