e14ba723db23b710114941f88ee861d691e9f1e0
JungSWon <sw.onandon@gmail.com>
2020-11-12 14:49:15 +0900

    [feat] UI 껍데기 제작

---
+++ web-FE/src/components/NewIssue/NewIssueMain.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NewIssueMain = () => (
+  <MainContainer>
+    <Div>
+      <Img />
+    </Div>
+    <Div />
+    <Div>
+
+    </Div>
+  </MainContainer>
+
+);
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export default NewIssueMain;
