e8ab46f4f82ffed54c6f8ba6e23592587fdd6805
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-12 11:06:07 +0900

    [feat] 이슈 하나를 id로 가져오는 query 추가

--- web-BE/config/query.js
+++ web-BE/config/query.js
@@ -56,6 +56,23 @@ module.exports = {
       JOIN User ON Issue.writer_id = User.user_id
       left JOIN (SELECT issue_id, COUNT(user_id) as assign_count FROM Assignee WHERE user_id=? group by issue_id) as User_Assign ON Issue.issue_id = User_Assign.issue_id
       left JOIN (SELECT issue_id, COUNT(writer_id) as comment_count FROM Comment WHERE writer_id=? group by issue_id) as User_Write ON Issue.issue_id = User_Write.issue_id;`,
+  selectIssueById: `SELECT
+      Issue.issue_id,
+      Issue.title,
+      Milestone.milestone_id,
+      Milestone.title as milestone_title,
+      Issue.write_time,
+      Issue.is_open,
+      User.user_id as writer_id,
+      User.username as writer,
+      ifnull(User_Assign.assign_count, 0) as is_assigned,
+      ifnull(User_Write.comment_count, 0) as is_mentioned
+    FROM Issue
+      LEFT JOIN Milestone ON Milestone.milestone_id = Issue.milestone_id
+      JOIN User ON Issue.writer_id = User.user_id
+      left JOIN (SELECT issue_id, COUNT(user_id) as assign_count FROM Assignee WHERE user_id=? group by issue_id) as User_Assign ON Issue.issue_id = User_Assign.issue_id
+      left JOIN (SELECT issue_id, COUNT(writer_id) as comment_count FROM Comment WHERE writer_id=? group by issue_id) as User_Write ON Issue.issue_id = User_Write.issue_id
+    WHERE Issue.issue_id = ?;`,
   updateIssueTitle: "UPDATE Issue SET title=? WHERE issue_id=?",
   insertIssueMilestone: "UPDATE Issue SET milestone_id=? WHERE issue_id=?",
   deleteIssueMilestone: "UPDATE Issue SET milestone_id=NULL WHERE issue_id=?",
