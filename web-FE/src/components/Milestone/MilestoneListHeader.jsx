1ab6c1efb7a8a109cd80892331c310f74c9fb65b
Seunghyo <seunghyoku@gmail.com>
2020-11-12 11:11:00 +0900

    [feat] 마일스톤 list 나오게끔 추가함
    - MilestoneListItem: 마일스톤 하나의 정보를 담은 리스트
    
    - MilestoneListHeader: 마일스톤표의 헤더에서 몇 개 open/close 되었는지 확인 가능
    
    -MilestoneListBoard: header와 item 불러와서 보여줌

---
+++ web-FE/src/components/Milestone/MilestoneListHeader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MilestoneListHeader = () => (
+  <FlexRowDiv>
+    <P>2 Open</P>
+    <P>0 Closed</P>
+  </FlexRowDiv>
+);
+
+const FlexRowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 50px;
+  background-color: #ececec;
+  border-radius: 10px 10px 0 0;
+  border: 1px solid #e1e4e8;
+`;
+
+const P = styled.span`
+  margin: 16px;
+`;
+
+export default MilestoneListHeader;
