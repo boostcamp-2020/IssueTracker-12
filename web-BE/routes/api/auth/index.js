024b1dfac84943a0678a6f767a7e2ebdb6a859da
JungSWon <sw.onandon@gmail.com>
2020-11-12 18:20:47 +0900

    [feat] apple login 을 위한 api 작성 - username, social로 해당 유저가 DB에 있는지 없는지 판단

--- web-BE/routes/api/auth/index.js
+++ web-BE/routes/api/auth/index.js
@@ -3,10 +3,15 @@ const authController = require("./controller");
 const passport = require("passport");
 
 router.get("/oauth", authController.getUserInfo);
+router.get("/check", authController.getUserInfoByUsername);
 router.post("/", authController.addUser);
 router.post("/token", authController.getToken);
-router.get('/', passport.authenticate("jwt", { session: false }), (req, res) => {
-  res.status(200).json(req.user);
-});
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.status(200).json(req.user);
+  }
+);
 
 module.exports = router;
