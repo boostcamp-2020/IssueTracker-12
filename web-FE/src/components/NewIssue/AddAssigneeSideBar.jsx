0ec4b5390b0307465b18d1a049366a2cf91270cc
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-12 17:26:10 +0900

    [feat] assignee 추가하는 sidebar 추가

---
+++ web-FE/src/components/NewIssue/AddAssigneeSideBar.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as GearIcon } from '@Images/gear.svg';
+import { UserContext } from '@Stores/UserStore';
+import AssigneeSelector from './AssigneeSelector';
+
+const AddAssigneeSideBar = () => {
+  const [assginess, setAssignees] = useState([]);
+  const { userState } = useContext(UserContext);
+  const getUsername = (userId) => userState.find((user) => user.user_id === userId).username;
+  return (
+    <Div>
+      <FlexColumn>
+        <AssigneeSelector
+          selected={assginess}
+          setSelected={setAssignees}
+          userState={userState}
+        />
+        <GearIcon
+          width="16px"
+          height="16px"
+        />
+      </FlexColumn>
+      {
+        assginess.map((assginee) => (<Item key={assginee}>{getUsername(assginee)}</Item>))
+      }
+    </Div>
+  )
+}
+
+const FlexColumn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const Item = styled.div`
+  padding: 10px 10px;
+  width: 100%;
+  box-sizing:border-box;
+  font-size: 14px;
+`;
+
+const Div = styled.div`
+  width: 280px;
+  min-height: 60px;
+  padding: 20px;
+  border-bottom: 1px solid lightgrey;
+  box-sizing:border-box;
+`;
+
+export default AddAssigneeSideBar;
---
+++ web-FE/src/components/NewIssue/AddLabelSideBar.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as GearIcon } from '@Images/gear.svg';
+import { LabelContext } from '@Stores/LabelStore';
+import LabelSelector from './LabelSelector';
+import LabelBadge from '@Components/commons/LabelBadge';
+
+const AddLabelSideBar = () => {
+  const [labels, setLabels] = useState([]);
+  const { labelState } = useContext(LabelContext);
+  const getLabelObjectById = (labelId) => labelState.find((label) => label.label_id === labelId);
+  return (
+    <Div>
+      <FlexColumn>
+        <LabelSelector
+          selected={labels}
+          setSelected={setLabels}
+          labelState={labelState}
+        />
+        <GearIcon
+          width="16px"
+          height="16px"
+        />
+      </FlexColumn>
+      <Item>
+        {
+          labels.map((labelId) => {
+            const label = getLabelObjectById(labelId);
+            return (
+              <LabelBadge
+                key={labelId}
+                name={label.label_name}
+                color={label.color}
+                margin="0 5px 5px 0"
+              />
+            );
+          })
+        }
+      </Item>
+    </Div>
+  )
+}
+
+const FlexColumn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px 10px;
+  width: 100%;
+  box-sizing:border-box;
+  font-size: 14px;
+`;
+
+const Div = styled.div`
+  width: 280px;
+  min-height: 60px;
+  padding: 20px;
+  border-bottom: 1px solid lightgrey;
+  box-sizing:border-box;
+`;
+
+export default AddLabelSideBar;
