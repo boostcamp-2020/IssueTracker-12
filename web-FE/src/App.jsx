ed17a10467e574148d6f985337a8f3844bf773e1
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-12 17:24:18 +0900

    [feat] app.js 에 유저, 마일스톤, 레이블 store 추가

--- web-FE/src/App.jsx
+++ web-FE/src/App.jsx
@@ -7,18 +7,27 @@ import Label from '@Components/Label';
 import Milestone from '@Components/Milestone';
 import IssueListPage from '@Components/Issue';
 import NewIssue from '@Components/NewIssue';
+import UserStore from '@Stores/UserStore';
+import LabelStore from '@Stores/LabelStore';
+import MilestoneStore from '@Stores/MilestoneStore';
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path='/login' component={Login} />
-      <Route path='/label' component={Label} />
-      <Route path='/milestone' component={Milestone} />
-      <Route exact path='/issue' component={IssueListPage} />
-      <Route exact path='/newIssue' component={NewIssue} />
-      <Route path='/' component={Home} />
-    </Switch>
-  </BrowserRouter>
+  <UserStore>
+    <MilestoneStore>
+      <LabelStore>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/label' component={Label} />
+            <Route path='/milestone' component={Milestone} />
+            <Route exact path='/issue' component={IssueListPage} />
+            <Route exact path='/newIssue' component={NewIssue} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </LabelStore>
+    </MilestoneStore>
+  </UserStore>
 );
 
 export default App;
