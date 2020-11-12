c3b91f6c9eecbbbb0d8e494e9b903c18482b75d2
JungSWon <sw.onandon@gmail.com>
2020-11-12 19:25:01 +0900

    [feat] textarea, tab UX

--- web-FE/src/components/NewIssue/InputDiv.jsx
+++ web-FE/src/components/NewIssue/InputDiv.jsx
@@ -1,35 +1,39 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
-import { createEditor } from 'slate';
-import { Slate, Editable, withReact } from 'slate-react';
+import Button from '@Common/Button';
 
 const InputDiv = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const [onWrite, setWrite] = useState('true');
+  const [onPreview, setPreview] = useState('false');
 
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ]);
+  const selectTab = (e) => {
+    const target = e.target.innerText;
+    if (target === 'Write') {
+      setWrite('true');
+      setPreview('false');
+      return;
+    }
+    setWrite('false');
+    setPreview('true');
+  };
 
   return (
     <MainContainer>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.23.0/polyfill.min.js" />
       <Title placeholder="Title" />
       <Header>
-        <P>Write</P>
-        <P>Preview</P>
+        <P onClick={selectTab} select={onWrite}>Write</P>
+        <P onClick={selectTab} select={onPreview}>Preview</P>
       </Header>
       <CommentContainer>
-        <Slate editor={editor} value={value} onChange={(newValue) => setValue(newValue)}>
-          <Editable />
-        </Slate>
+        <TextArea placeholder="Leave a comment" />
       </CommentContainer>
+      <SubmitButton> Submit new issue </SubmitButton>
     </MainContainer>
   );
 };
+
+const border = 'border: 1px solid #e1e4e8;';
 
 const MainContainer = styled.div`
   display: flex;
@@ -38,7 +42,7 @@ const MainContainer = styled.div`
   width: 70%;
   height: 400px;
   border-radius: 5px;
-  border: 1px solid #e1e4e8;
+  ${border}
 `;
 
 const Title = styled.input`
@@ -46,7 +50,7 @@ const Title = styled.input`
   height: 30px;
   margin: 5px;
   border-radius: 5px;
-  border: 1px solid #e1e4e8;
+  ${border}
   background-color: #f6f8fa;
   font-size: 18px;
   font-weight: 100;
@@ -54,8 +58,6 @@ const Title = styled.input`
 `;
 
 const Header = styled.div`
-  position: relative;
-  top: 1px;
   display:flex;
   flex-direction: row;
   height: 60px;
@@ -65,31 +67,52 @@ const Header = styled.div`
 `;
 
 const P = styled.p`
+  position: relative;
+  top: 1px;
   width: 75px;
   height: 25px;
-  border: 1px solid #e1e4e8;
+  ${border};
+  border-color : ${(props) => {
+    if (props.select === 'true') return '#e1e4e8;';
+    return 'transparent;';
+  }};
   border-top-left-radius: 4px;
   border-top-right-radius: 4px;
   border-bottom: 0;
   text-align: center;
   margin-bottom:0px;
   padding-top: 12px;
-  background-color: #ffffff;
+  background-color: ${(props) => {
+    if (props.select === 'true') return '#ffffff';
+    return 'transparant';
+  }};
 `;
 
 const CommentContainer = styled.div`
   display: flex;
   justify-content: center;
-  border: 1px solid #e1e4e8;
+  ${border}
   width:100%;
   height:70%;
   margin:0px;
 `;
 
 const TextArea = styled.textarea`
-  width: 90%;
-  height: 90%;
+  width: 87%;
+  height: 70%;
+  padding: 20px;
   margin-top: 10px;
+  ${border}
+  border-radius: 5px;
+  background-color: #f6f8fa;
+`;
+
+const SubmitButton = styled(Button)`
+  height: 25px;
+  display: flex;
+  align-items: center;
+  align-self: flex-end;
+  margin: 8px 10px;
 `;
 
 export default InputDiv;
